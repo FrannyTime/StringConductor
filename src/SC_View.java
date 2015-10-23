db9615ac3a0af3d7f6dedd02866d1c6de69d4fce
Daniel Tam <mistahtam@gmail.com>
2015-10-22 21:22:21 -0700

    Import button finished

--- src/SC_View.java
+++ src/SC_View.java
@@ -6,21 +6,19 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class SC_View extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
 
         BorderPane bp = new BorderPane();
 
@@ -32,7 +30,7 @@ public class SC_View extends Application {
 
         bp.setCenter(inputField);
         bp.setAlignment(inputField, Pos.CENTER_RIGHT);
-        bp.setMargin(inputField, new Insets(12,12,12,12));
+        bp.setMargin(inputField, new Insets(12,200,12,200));
 
         //Button to check leap year
         Button checkButton = new Button("Check");
@@ -59,10 +57,42 @@ public class SC_View extends Application {
         bp.setAlignment(checkButton, Pos.BOTTOM_CENTER);
         bp.setMargin(checkButton, new Insets(12,12,12,12));
 
-        Scene scene = new Scene(bp, 300, 200);
-        primaryStage.setTitle("Leap Year Checker");
+        MenuBar menuBar = new MenuBar();
+        final Menu menu1 = new Menu("File");
+        final Menu menu2 = new Menu("Options");
+        final Menu menu3 = new Menu("Help");
+        Menu menu4 = new Menu("Import");
+
+        MenuItem hi = new MenuItem("hi");
+        MenuItem importBut = new MenuItem("Import");
+
+        menu1.getItems().addAll(hi, importBut);
+
+        hi.setOnAction(e -> yearLabel.setText("Hi"));
+        importBut.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            final File file = fileChooser.showOpenDialog(primaryStage);
+
+            System.out.println("getCurrentDirectory(): " + file.getPath());
+            //System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
+        });
+
+
+
+        menuBar.getMenus().addAll(menu1, menu2, menu3, menu4);
+
+        bp.setTop(menuBar);
+
+        Scene scene = new Scene(bp, 700, 500);
+        primaryStage.setTitle("String Conductor");
         primaryStage.setScene(scene);
         primaryStage.show();
 
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
