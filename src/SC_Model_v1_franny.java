50f8a83722264c3d18f80f0a8e19fa96dca9b7fb
Francisco McGee <fmcgee@natera.com>
2015-10-24 15:03:32 -0700

    some code for DocumentParser

--- src/SC_Model_v1_franny.java
+++ src/SC_Model_v1_franny.java
@@ -1,38 +1,98 @@
-import java.io.*;
-import java.io.FileReader;
-import java.text.BreakIterator;
 
-/**
- * Created by Natera on 10/22/15.
- */
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.BreakIterator;
+import java.util.*;
+
+
+
 public class SC_Model_v1_franny
 {
-/**
- * Created by PeteCurtis on 10/22/15.
- */
+    private static final String PHRASE_DELIMITER = " ";
+    public static final Integer MAX_PHRASE_LENGTH = 10;
+    public static final Integer MIN_PHRASE_LENGTH = 2;
+    public HashMap<String, Integer> phraseTable = new HashMap<String, Integer>();
 
+    BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+    String document = fileReader(location);
+    iterator.setText(document);
+    int start = iterator.first();
 
-    static String readFile (String fileName)throws IOException
+    for (int end = iterator.next(); end != BreakIterator.DONE;
+    start = end, end = iterator.next())
     {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        try
-        {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
+        String sentence = document.substring(start, end);
+        String[] sentenceArray = sentence.split("\\s+");
+        processSentenceArray(sentenceArray);
 
-            while (line != null)
-            {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-            return sb.toString();
-        }
-        finally
+    }
+
+    private static void processSentenceArray(String[] sentenceArray)
+    {
+        int maxPhraseLength = MAX_PHRASE_LENGTH;
+
+        if (sentenceArray.length < maxPhraseLength)
         {
-            br.close();
+            maxPhraseLength = sentenceArray.length;
+        }
+
+        Integer phraseLength = MIN_PHRASE_LENGTH;
+
+        for (phraseLength; phraseLength < maxPhraseLength; phraseLength++)
+        {
+            chopSentenceArray(sentenceArray, phraseLength)
         }
     }
+
+    private static void chopSentenceArray(String[] sentenceArray, Integer phraseLength)
+    {
+        int phraseStartPosition = 0;
+
+        while (phraseStartPosition <= sentenceArray.length )
+        {
+            try
+            {
+                if (sentenceArray[phraseStartPosition + (phraseLength-1)] != null)
+                {
+                    int k = phraseStartPosition;
+                    String hashEntry = new String();
+                    for (k = phraseStartPosition; k < phraseStartPosition + (phraseLength - 1); k++)
+                    {
+                        hashentry += sentenceArray[k] + PHRASE_DELIMITER;
+                    }
+
+                    addToPhraseTable(hashEntry);
+                    phraseTable.put(hashEntry, number);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException aiobe)
+            {
+                System.out.println(aiobe);
+            }
+
+            phraseStartPosition++;
+        }
+    }
+
+
+    private static void addToPhraseTable(String hashEntry)
+    {
+        if (phraseTable.containsKey(hashEntry))
+        {
+            phraseTable.put(hashEntry, phraseTable.get(hashEntry) + 1);
+        }
+        else
+        {
+            phraseTable.put(hashEntry, 1);
+        }
+    }
+
+
 
     public static void main (String[] args) throws IOException, FileNotFoundException
     {
@@ -43,9 +103,6 @@ public class SC_Model_v1_franny
 
         DocumentParser myParser = new DocumentParser();
         myParser.documentParser(myString);
-
-
-
     }
 }
 
