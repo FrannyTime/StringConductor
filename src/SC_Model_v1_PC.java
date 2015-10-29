792046d5985931ea27d8667325ff82b14fc4849a
Pete Curtis <pmjcurtis@gmail.com>
2015-10-28 23:33:08 -0700

    A lot of stuff with Fransico.  Mostly in Document Parser, lots of commenting

--- src/SC_Model_v1_PC.java
+++ src/SC_Model_v1_PC.java
@@ -1,31 +1,131 @@
-import java.io.File;
+
+
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-/**
- * Created by PeteCurtis on 10/22/15.
- */
-public class SC_Model_v1_PC {
-    /**
-     * Created by PeteCurtis on 10/22/15.
-     */
-
-        public static void main(String[] args) throws FileNotFoundException {
-            //listen for click on import
-            //once you have a file pass that to the model
-            //the model then has a file object
-
-            //make new objects
-            File sampleFile = new File("/Users/PeteCurtis/git/StringConductor/src/stuff.txt");
-            java.io.FileReader fr = new FileReader(sampleFile);
-//            System.out.println(fr);
-
-//            fr.textScanner();
-//            DocumentParser pp = new DocumentParser();
-//
-//            //pass new document from FileReader into phrase Parser
-//            pp.documentParser(fr.getDocument());
+import java.io.IOException;
+import java.text.BreakIterator;
+import java.util.*;
+import java.lang.StringBuilder;
 
 
+
+public class SC_Model_v1_PC
+{
+    private static final String PHRASE_DELIMITER = " ";
+    public static final Integer MAX_PHRASE_LENGTH = 10;
+    public static final Integer MIN_PHRASE_LENGTH = 2;
+    public static HashMap<String, Integer> phraseTable = new HashMap<>();
+    public static final String FILE_LOCATION = "/Users/PeteCurtis/git/StringConductor/src/stuff.txt";
+
+    public static String readFile(String location) throws FileNotFoundException
+    {
+        String theDocument = "";
+
+        java.io.File docFile = new java.io.File(location);
+        try
+        {
+            Scanner input = new Scanner(docFile);
+            while (input.hasNext())
+            {
+                theDocument += input.nextLine();
+            }
+            input.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("FileNotFound");
+        } //catch block
+
+        return theDocument;
+    }
+
+    private static void processSentenceArray(String[] sentenceArray)
+    {
+        int maxPhraseLength = MAX_PHRASE_LENGTH;
+
+        if (sentenceArray.length < maxPhraseLength)
+        {
+            maxPhraseLength = sentenceArray.length;
+        }
+
+        int phraseLength;
+
+        for (phraseLength = MIN_PHRASE_LENGTH; phraseLength < maxPhraseLength; phraseLength++)
+        {
+            chopSentenceArray(sentenceArray, phraseLength);
         }
     }
+
+    private static void chopSentenceArray(String[] sentenceArray, Integer phraseLength)
+    {
+        int phraseStartPosition = 0;
+
+        while (phraseStartPosition <= sentenceArray.length)
+        {
+            try
+            {
+                if (sentenceArray[phraseStartPosition + (phraseLength-1)] != null)
+                {
+                    int k = phraseStartPosition;
+                    String hashEntry = new String();
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    for (k = phraseStartPosition; k < phraseStartPosition + (phraseLength - 1); k++)
+                    {
+                        stringBuilder.append(sentenceArray[k] + PHRASE_DELIMITER);
+                    }
+
+                    hashEntry = stringBuilder.toString();
+                    addToPhraseTable(hashEntry);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException aiobe)
+            {
+                System.out.println(aiobe);
+            }
+
+            phraseStartPosition++;
+        }
+    }
+
+    private static void addToPhraseTable(String hashEntry)
+    {
+        if (phraseTable.containsKey(hashEntry))
+        {
+            phraseTable.put(hashEntry, phraseTable.get(hashEntry) + 1);
+        }
+        else
+        {
+            phraseTable.put(hashEntry, 1);
+        }
+    }
+
+    public static void main (String[] args) throws IOException
+    {
+        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+
+        String document = readFile(FILE_LOCATION);
+        iterator.setText(document);
+        int start = iterator.first();
+
+        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next())
+        {
+            String sentence = document.substring(start, end);
+            String[] sentenceArray = sentence.split("\\s+");
+            processSentenceArray(sentenceArray);
+        }
+
+        for (Map.Entry<String, Integer> entry : phraseTable.entrySet())
+        {
+            System.out.println(entry);
+        }
+        System.out.println("bananas");
+    }
+}
+
+
+
 
---
+++ src/phraseTableComparator.java
@@ -0,0 +1,24 @@
+//import javax.xml.bind.SchemaOutputResolver;
+//import java.util.Comparator;
+//import java.util.HashMap;
+//import java.util.Hashtable;
+//import java.util.Map;
+//
+///**
+// * Created by PeteCurtis on 10/25/15.
+// */
+//public class phraseTableComparator implements Comparator{
+//
+//    Map map;
+//
+//    public phraseTableComparator(Map<String, Integer> map)
+//    {
+//        this.map = map;
+//    }
+////    public int compare(Map.Entry<String, Integer> map1, Map.Entry<String, Integer> map2)
+//    public int compare( Object mapA, Object mapB)
+//    {
+//        (Comparable) ValueA
+//        return map1.getValue().compareTo(map2.getValue());
+//    }
+//}
