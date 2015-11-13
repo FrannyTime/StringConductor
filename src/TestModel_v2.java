9f287d2741146f2cef9984e5df5abd6ceb2964d5
Francisco McGee <fmcgee@natera.com>
2015-11-12 19:46:40 -0800

    some code for DocumentParser

--- src/TestModel_v2.java
+++ src/TestModel_v2.java
@@ -1,26 +1,29 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.BreakIterator;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
+import javafx.util.Pair;
+import org.mozilla.universalchardet.UniversalDetector;
 
 /**
  * Created by Natera on 11/10/15.
  */
 public class TestModel_v2
 {
+    public static ArrayList<ArrayList<Pair<Integer, String>>> wordCountList = new ArrayList<>(50);
     public static final Integer MAX_PHRASE_LENGTH = 11;
     public static final Integer MIN_PHRASE_LENGTH = 4;
     private static final String PHRASE_DELIMITER = " ";
 
     public static String theDocument = "";
 
-    public static final String FILE_LOCATION = "/Users/Natera/Documents/CS/SC_text_new.txt";
+    public static final String FILE_LOCATION = "/Users/Natera/Documents/CS/SC_text_new_new16LittleEndian.txt";
 
     public static String readFile(String location) throws FileNotFoundException
     {
         java.io.File docFile = new java.io.File(location);
+
         try
         {
             Scanner input = new Scanner(docFile);
@@ -49,26 +52,65 @@ public class TestModel_v2
         iterator.setText(document);
         int start = iterator.first();
 
+        // starting here is file format detector code from UniversalDetector
+        UniversalDetector detector = new UniversalDetector(null);
+        byte[] buf = new byte[10000];
+        java.io.FileInputStream fis = new java.io.FileInputStream(FILE_LOCATION);
+
+        int nread;
+        while ((nread = fis.read(buf)) > 0 && !detector.isDone())
+        {
+            detector.handleData(buf, 0, nread);
+        }
+
+        detector.dataEnd();
+
+        String encoding = detector.getDetectedCharset();
+        if (encoding != null)
+        {
+            System.out.println("Detected encoding = " + encoding);
+        }
+        else
+        {
+            System.out.println("No encoding detected.");
+        }
+
+        detector.reset();
+        // the detector code ends here
+
         for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next())
         {
-
             String sentence = document.substring(start, end);
             System.out.println(sentence);
             String[] sentenceArray = sentence.split("\\s+");
             model.processSentenceArray(sentenceArray, MAX_PHRASE_LENGTH, MIN_PHRASE_LENGTH, PHRASE_DELIMITER);
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
-
-
         for (Map.Entry<String, Integer> entry : model.frequencyTable.entrySet())
         {
-//            if (entry.getKey() )
-//            System.out.println(entry);
             String s = entry.getValue() + "\t" + entry.getKey();
             System.out.println(s);
-           // stringBuilder.append(s);
+            Integer wordCount = countTheWords(entry.getKey());
+            System.out.println(wordCount);
         }
-       // view.runApp(args);
+
+        for (ArrayList<Pair<Integer, String>> list : wordCountList)
+        {
+            for (Pair<Integer, String> phrase : list)
+            {
+                System.out.println(phrase.getKey() + "\t" + phrase.getValue());
+            }
+        }
     }
+
+    private static Integer countTheWords(String phrase)
+    {
+        Integer wordCount;
+        String[] sentenceArray = phrase.split("\\s+");
+        wordCount = sentenceArray.length;
+
+        return wordCount;
+    }
+
+
 }
