f32f85e0c1d68d12723afd73c6cbcd3a8ef76617
Francisco McGee <fmcgee@natera.com>
2015-11-12 20:18:09 -0800

    some code for DocumentParser

--- src/SC_Controller_v1_franny.java
+++ src/SC_Controller_v1_franny.java
@@ -14,9 +14,11 @@ public class SC_Controller_v1_franny
     public static final Integer MIN_PHRASE_LENGTH = 2;
     private static final String PHRASE_DELIMITER = " ";
     public static String theDocument = "";
+
+
+
     private static SC_View view = new SC_View();
     private static SC_Model_v2_franny model = new SC_Model_v2_franny();
-
 
     public static final String FILE_LOCATION = "/Users/Natera/Documents/CS/SC_text.txt";
 
@@ -70,23 +72,37 @@ public class SC_Controller_v1_franny
         view.runApp(args);
     }
 
-    public static ArrayList<String> getWordCountFreq(Integer wordCount)
-    {
-        String returnString = new String();
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (String entry : )
-
-        returnString = model.getWordCountResults(wordCount);
-
-        return returnString;
-    }
-
-    public
+//    public static ArrayList<String> getWordCountFreq(Integer wordCount)
+//    {
+//        String returnString = new String();
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//        for (String entry : )
+//
+//        returnString = model.getWordCountResults(wordCount);
+//
+//        return returnString;
+//    }
+//
+//    public static String getPhraseFreq
 
     public static String getImportResults(String phrase) throws IOException
     {
         String returnString = new String();
+
+        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+
+        String document = readFile(FILE_LOCATION);
+        iterator.setText(document);
+        int start = iterator.first();
+
+        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next())
+        {
+            String sentence = document.substring(start, end);
+            String[] sentenceArray = sentence.split("\\s+");
+            model.processSentenceArray(sentenceArray, MAX_PHRASE_LENGTH, MIN_PHRASE_LENGTH, PHRASE_DELIMITER);
+        }
+
         StringBuilder stringBuilder = new StringBuilder();
 
         for (Map.Entry<String, Integer> entry : model.frequencyTable.entrySet())
@@ -96,27 +112,12 @@ public class SC_Controller_v1_franny
             stringBuilder.append(s);
         }
 
-        String boogers = "boogers";
+//        String boogers = "boogers";
         returnString = stringBuilder.toString();
 
-        return boogers;
+        return returnString;
     }
 
 }
 //
-//SC_Model_v2_franny model = new SC_Model_v2_franny();
-//String returnString = new String();
-//
-//BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
-//
-//String document = readFile(inputFileLocation);
-//iterator.setText(document);
-//        int start = iterator.first();
-//
-//        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next())
-//        {
-//        String sentence = document.substring(start, end);
-//        String[] sentenceArray = sentence.split("\\s+");
-//        model.processSentenceArray(sentenceArray, MAX_PHRASE_LENGTH, MIN_PHRASE_LENGTH, PHRASE_DELIMITER);
-//        }
-//
+
