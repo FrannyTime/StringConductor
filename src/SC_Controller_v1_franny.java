657df88b68e1511a7370f9010e80417e8427ed2c
Francisco McGee <fmcgee@natera.com>
2015-11-12 20:19:31 -0800

    some code for DocumentParser

--- src/SC_Controller_v1_franny.java
+++ src/SC_Controller_v1_franny.java
@@ -7,8 +7,7 @@ import java.text.BreakIterator;
 import java.util.*;
 import java.lang.StringBuilder;
 
-public class SC_Controller_v1_franny
-{
+public class SC_Controller_v1_franny {
 
     public static final Integer MAX_PHRASE_LENGTH = 11;
     public static final Integer MIN_PHRASE_LENGTH = 2;
@@ -16,61 +15,52 @@ public class SC_Controller_v1_franny
     public static String theDocument = "";
 
 
-
-    private static SC_View view = new SC_View();
+//    private static SC_View view = new SC_View();
     private static SC_Model_v2_franny model = new SC_Model_v2_franny();
 
     public static final String FILE_LOCATION = "/Users/Natera/Documents/CS/SC_text.txt";
 
-    public static String readFile(String location) throws FileNotFoundException
-    {
+    public static String readFile(String location) throws FileNotFoundException {
         java.io.File docFile = new java.io.File(location);
-        try
-        {
+        try {
             Scanner input = new Scanner(docFile);
 
-            while (input.hasNext())
-            {
+            while (input.hasNext()) {
                 theDocument += input.nextLine();
             }
             input.close();
-        }
-        catch(FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.out.println("FileNotFound");
         }
 
         return theDocument;
     }
+//
+//    public static void main(String[] args) throws IOException {
+//        SC_Model_v2_franny model = new SC_Model_v2_franny();
+//
+//        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+//
+//        String document = readFile(FILE_LOCATION);
+//        iterator.setText(document);
+//        int start = iterator.first();
+//
+//        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next()) {
+//            String sentence = document.substring(start, end);
+//            String[] sentenceArray = sentence.split("\\s+");
+//            model.processSentenceArray(sentenceArray, MAX_PHRASE_LENGTH, MIN_PHRASE_LENGTH, PHRASE_DELIMITER);
+//        }
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//
+//        for (Map.Entry<String, Integer> entry : model.frequencyTable.entrySet()) {
+//            String s = entry.getValue() + "\t" + entry.getKey();
+//            stringBuilder.append(s);
+//        }
+//        view.runApp(args);
+//    }
 
-    public static void main (String[] args) throws IOException
-    {
-        SC_View view = new SC_View();
-        SC_Model_v2_franny model = new SC_Model_v2_franny();
-
-        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
-
-        String document = readFile(FILE_LOCATION);
-        iterator.setText(document);
-        int start = iterator.first();
-
-        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator.next())
-        {
-            String sentence = document.substring(start, end);
-            String[] sentenceArray = sentence.split("\\s+");
-            model.processSentenceArray(sentenceArray, MAX_PHRASE_LENGTH, MIN_PHRASE_LENGTH, PHRASE_DELIMITER);
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-
-        for (Map.Entry<String, Integer> entry : model.frequencyTable.entrySet())
-        {
-            String s = entry.getValue() + "\t" + entry.getKey();
-            stringBuilder.append(s);
-        }
-        view.runApp(args);
-    }
 
 //    public static ArrayList<String> getWordCountFreq(Integer wordCount)
 //    {
